0c1a56bafc6ddd73520374e1662bfee4f2cdd2b1
Younes A. <younesallachi@hotmail.com>
2024-05-03 19:11:23 +0200

    correction mauvais emplacement fichier racine

---
+++ src/test/java/ICC/TFE/Vitamiel/VitamielApplicationTests.java
@@ -0,0 +1,13 @@
+package ICC.TFE.Vitamiel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitamielApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
