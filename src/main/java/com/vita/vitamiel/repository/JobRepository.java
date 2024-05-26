e5f7e818e226a108268172c9334f549ceb4dbdd8
Younes A. <younesallachi@hotmail.com>
2024-05-26 04:54:33 +0200

    développement interface repository

--- src/main/java/com/vita/vitamiel/repository/JobRepository.java
+++ src/main/java/com/vita/vitamiel/repository/JobRepository.java
@@ -1,4 +1,14 @@
 package com.vita.vitamiel.repository;
 
-public interface JobRepository {
+import com.vita.vitamiel.model.Job;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.LinkedList;
+import java.util.UUID;
+
+public interface JobRepository extends JpaRepository<Job, UUID> {
+
+    @Query("SELECT j FROM Job j")
+    LinkedList<Job> findAllJob();
 }
