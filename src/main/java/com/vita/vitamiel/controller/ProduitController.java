b0bbf416ec7b0d30cba0fdafee935baddf73182c
Younes A. <younesallachi@hotmail.com>
2024-05-27 23:39:03 +0200

    mise au point de endpoint supression

--- src/main/java/com/vita/vitamiel/controller/ProduitController.java
+++ src/main/java/com/vita/vitamiel/controller/ProduitController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.LinkedList;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/produit")
@@ -38,5 +39,14 @@ public class ProduitController {
         return createProduit;
     }
 
+
+
+    @DeleteMapping("/{produitid}")
+    public String deleteProduit(@PathVariable UUID produitid) throws Exception{
+
+        produitService.deleteProduit(produitid);
+
+        return "le produit a été supprimé";
+    }
 
 }
