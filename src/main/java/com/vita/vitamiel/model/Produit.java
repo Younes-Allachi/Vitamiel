0933bae0f486e286f7fbf543f16646b959334156
Younes A. <younesallachi@hotmail.com>
2024-05-27 21:42:08 +0200

    création Model Produit

---
+++ src/main/java/com/vita/vitamiel/model/Produit.java
@@ -0,0 +1,30 @@
+package com.vita.vitamiel.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "produit")
+public class Produit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String nom;
+    private String description;
+    private String origine;
+    private double poids;
+    private double prix;
+
+    public void obtenirDetailsProduit(){
+
+    }
+}
