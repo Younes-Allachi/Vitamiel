214be71b37443cca0e9e91571d9464d7f569edcd
Younes A. <younesallachi@hotmail.com>
2024-05-31 04:12:22 +0200

    création interface PersonneRepository

---
+++ src/main/java/com/vita/vitamiel/repository/PersonneRepository.java
@@ -0,0 +1,14 @@
+package com.vita.vitamiel.repository;
+
+import com.vita.vitamiel.model.Personne;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.LinkedList;
+import java.util.UUID;
+
+public interface PersonneRepository extends JpaRepository<Personne, UUID> {
+
+    @Query("SELECT p FROM Produit p")
+    LinkedList<Personne> findAllPersonne();
+}
