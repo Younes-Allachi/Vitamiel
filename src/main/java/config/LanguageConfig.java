f6c13b0c1e235ccdc5d3db4f3c6efee2a82fcf78
Younes A. <younesallachi@hotmail.com>
2024-06-05 04:34:16 +0200

    création configuration pour les langues

---
+++ src/main/java/config/LanguageConfig.java
@@ -0,0 +1,13 @@
+package config;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+
+public class LanguageConfig {
+
+    public static final List<Locale> locales =Arrays.asList(
+            new Locale("en"),
+            new Locale("fr")
+    );
+}
