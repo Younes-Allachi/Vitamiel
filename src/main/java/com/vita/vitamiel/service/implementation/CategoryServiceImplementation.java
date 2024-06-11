2ffb8c6fbb8cae6d85055c6e562e108a0de77aa6
Younes A. <younesallachi@hotmail.com>
2024-06-11 03:13:02 +0200

    ajout du service suppression

--- src/main/java/com/vita/vitamiel/service/implementation/CategoryServiceImplementation.java
+++ src/main/java/com/vita/vitamiel/service/implementation/CategoryServiceImplementation.java
@@ -1,11 +1,19 @@
 package com.vita.vitamiel.service.implementation;
 
 import com.vita.vitamiel.model.Category;
+import com.vita.vitamiel.repository.CategoryRepository;
 import com.vita.vitamiel.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Service
 public class CategoryServiceImplementation implements CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Override
     public Category createcategory(Category category) {
         return null;
@@ -23,6 +31,10 @@ public class CategoryServiceImplementation implements CategoryService {
 
     @Override
     public void deleteCategory(UUID id) throws Exception {
+
+        findCategoryById(id);
+
+        categoryRepository.deleteById(id);
 
     }
 }
