cf22f23e5d917bbe263737d8282bebc0d9e82630
Younes A. <younesallachi@hotmail.com>
2024-06-11 03:09:00 +0200

    développement service Category

---
+++ src/main/java/com/vita/vitamiel/service/implementation/CategoryServiceImplementation.java
@@ -0,0 +1,28 @@
+package com.vita.vitamiel.service.implementation;
+
+import com.vita.vitamiel.model.Category;
+import com.vita.vitamiel.service.CategoryService;
+
+import java.util.UUID;
+
+public class CategoryServiceImplementation implements CategoryService {
+    @Override
+    public Category createcategory(Category category) {
+        return null;
+    }
+
+    @Override
+    public Category findCategoryById(UUID id) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Category updateCategory(Category category, UUID id) throws Exception {
+        return null;
+    }
+
+    @Override
+    public void deleteCategory(UUID id) throws Exception {
+
+    }
+}
